9cc63a0507395411d1baecd7525b5ee333d1d9ec
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-03-15 14:08:56 +0300

    Fixed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
 
     public static boolean isPositiveNumber(String str) {
-         return Integer.parseInt(str) > 0;
+         return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
